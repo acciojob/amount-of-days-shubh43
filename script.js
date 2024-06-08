4a2e53c994d6bac545e513d5af3bbaa9a30b8c36
shubh43 <41281712+shubh43@users.noreply.github.com>
2024-06-08 08:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function days(date) {
+	if((date%4==0&&date%100!=0)||(date%400==0))
+	{
+		return 366;
+	}
+	return 365;
+}
+
+let date=prompt("daysOfyear");
+alert(days(date));
